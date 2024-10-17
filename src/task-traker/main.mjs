660b784be2ca15c501902471dc91648460b3f7f7
Alam Guardin <alamguardin@gmail.com>
2024-10-16 22:34:20 -0500

    feat(task-traker): check if json file exists

--- src/task-traker/main.mjs
+++ src/task-traker/main.mjs
@@ -1,1 +1,16 @@
-console.log(process.argv);
+import { CliApp } from './src/cliApp.mjs';
+import * as fs from 'node:fs/promises';
+
+async function main() {
+	try {
+		await fs.access('db.json', fs.constants.F_OK);
+		const filePath = new URL('./db.json', import.meta.url);
+		const content = await fs.readFile(filePath, { encoding: 'utf-8' });
+		console.log(JSON.parse(content));
+	} catch (err) {
+		const filePath = new URL('./db.json', import.meta.url);
+		const newFile = await fs.writeFile(filePath, '[]', 'utf-8');
+	}
+}
+
+main();
