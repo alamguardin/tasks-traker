0e6f2698fe411ca7e6a88d118bf65856322787ed
Alam Guardin <alamguardin@gmail.com>
2024-10-17 20:11:37 -0500

    feat(task-traker): get args from the command line

--- src/task-traker/main.mjs
+++ src/task-traker/main.mjs
@@ -1,16 +1,24 @@
 import { CliApp } from './src/cliApp.mjs';
 import * as fs from 'node:fs/promises';
 
-async function main() {
+async function main(args) {
+	const cli = new CliApp();
+
 	try {
 		await fs.access('db.json', fs.constants.F_OK);
 		const filePath = new URL('./db.json', import.meta.url);
 		const content = await fs.readFile(filePath, { encoding: 'utf-8' });
+		cli.tasksList.tasks = JSON.parse(content);
 		console.log(JSON.parse(content));
 	} catch (err) {
 		const filePath = new URL('./db.json', import.meta.url);
-		const newFile = await fs.writeFile(filePath, '[]', 'utf-8');
+		await fs.writeFile(filePath, '[]', 'utf-8');
 	}
+
+	cli.parser(args);
+
+	const filePath = new URL('./db.json', import.meta.url);
+	fs.writeFile(filePath, JSON.stringify(cli.getTasks()), 'utf-8');
 }
 
-main();
+main(process.argv.slice(2));
